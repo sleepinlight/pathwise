d07b4301a858c62e18333fee106c6da131970111
Andy Carter <acarter264@gmail.com>
2026-01-23 04:41:43 -0600

    Initial Commit

---
+++ pathwise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pathwise
+//
+//  Created by Andy Carter on 1/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pathwise/pathwiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  pathwiseApp.swift
+//  pathwise
+//
+//  Created by Andy Carter on 1/23/26.
+//
+
+import SwiftUI
+
+@main
+struct pathwiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
